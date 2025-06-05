6c385520b42c3f049d86eac6b325dcf2298740a8
Liang Tan <liangtan@me.com>
2025-06-05 10:20:28 -0400

    use hashrouter to fix gh page routing

---
+++ src/components/tabs/AboutMe.tsx
@@ -0,0 +1,67 @@
+import { type FC } from "react";
+import Editor from "../Editor";
+import LineNumber from "../LineNumber";
+import {
+  Colon,
+  Comment,
+  Const,
+  EqualSign,
+  FunctionName,
+  Keyword,
+  Parentheses,
+  StringLiteral,
+  TypeName,
+  VariableName,
+} from "../Shared";
+import styles from "./AboutMe.module.scss";
+
+const lines = [
+  <Comment>// Hi there! It's-a-me 👋</Comment>,
+  <>
+    <Const>const</Const>{" "}
+    <VariableName>name</VariableName>{" "}
+    <EqualSign/>{" "}
+    <StringLiteral>"Liang Tan"</StringLiteral>;
+  </>,
+  <>
+    <Const>const</Const>{" "}
+    <VariableName>title</VariableName>{" "}
+    <EqualSign/>{" "}
+    <StringLiteral>"Chief Code Officer 🧠💻"</StringLiteral>;
+  </>,
+  <>
+    <Const>const</Const>{" "}
+    <VariableName>loves</VariableName>{" "}
+    <EqualSign/>{" "}[
+    <StringLiteral>"design curious"</StringLiteral>, <StringLiteral>"code"</StringLiteral>, <StringLiteral>"making weird ideas real"</StringLiteral>, <StringLiteral>"debugging at 3:21 PM"</StringLiteral>
+    ];
+  </>,
+  <>
+  	<br />
+  </>,
+  <>
+    <Keyword>function</Keyword> <FunctionName>sayHi</FunctionName><Parentheses/><Colon/> <TypeName>string</TypeName> {"{"}
+  </>,
+  <>
+    {"  "}
+    <Keyword>return</Keyword> <StringLiteral>"Thanks for visiting!"</StringLiteral>;
+  </>,
+  <>
+    {"}"}
+  </>,
+  <br/>
+];
+
+const AboutMe: FC = () => (
+  <div className={styles.container}>
+    <LineNumber count={lines.length + 1}></LineNumber>
+    <Editor>
+      {lines.map((line, idx) => (
+        <div key={idx}>{line}</div>
+      ))}
+      {/* <TypingEffect texts={["sayHi();"]}></TypingEffect> */}
+    </Editor>
+  </div>
+);
+
+export default AboutMe;
