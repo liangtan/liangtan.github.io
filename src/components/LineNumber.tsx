b9b9a9add5fb3ced34b627359c4f17464a0d324f
Liang Tan <liangtan@me.com>
2025-06-04 20:11:45 -0400

    refactoring

--- src/components/LineNumber.tsx
+++ src/components/LineNumber.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import type { FC } from "react";
 import { lineNumberStyle } from "../constants/styles";
 
 interface LineNumberProps {
   count: number;
 }
 
-const LineNumber: React.FC<LineNumberProps> = ({ count }: { count: number }) => {
+const LineNumber: FC<LineNumberProps> = ({ count }: { count: number }) => {
   return (
     <div style={lineNumberStyle}>
       {Array.from({ length: count }, (_, i) => (
