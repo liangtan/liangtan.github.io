d5da4815a4984412b9aa83dffe57185f09fec0dd
Liang Tan <liangtan@me.com>
2025-07-15 16:01:29 -0400

    Update contacts.ts

--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -1,4 +1,4 @@
-export const email = "liangtan@me.com";
+export const email = "reachliang@icloud.com";
 export const github = "https://github.com/liangtan";
 export const linkedin = "https://linkedin.com/in/liangtan521";
 export const portfolio = "https://liangtan.github.io";
